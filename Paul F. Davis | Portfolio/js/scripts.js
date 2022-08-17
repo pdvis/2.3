f572d63877677dd6f306e1f9f94e927e4b34721b
pdvis <108772101+pdvis@users.noreply.github.com>
2022-08-17 15:26:10 -0700

    delete

--- Paul F. Davis | Portfolio/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  }
-  else {
-    x.className = 'navtoggle';
-  }
-}
